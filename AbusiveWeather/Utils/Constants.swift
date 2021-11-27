64a54f0574b7314c4d7875282a080348a74409fb
Jon Sweeney <jonsweeney26@gmail.com>
2021-11-26 19:07:30 -0800

    Update API

--- AbusiveWeather/Utils/Constants.swift
+++ AbusiveWeather/Utils/Constants.swift
@@ -10,7 +10,7 @@ import Foundation
 struct K {
     struct Urls {
         static func weatherByCity(city: String) -> URL? {
-            return URL(string: "https://api.openweathermap.org/data/2.5/weather?q=\(city.escaped())&appid=ef0fd9866ca027e0dca474cee84c53be")
+            return URL(string: "https://api.openweathermap.org/data/2.5/weather?q=\(city.escaped())&appid=945dd898f76f99e17e0b49c5279215d4")
         }
         
         static func weatherUrlAsStringByIcon(icon: String) -> String {
