a386ffc1dab9c8bab9a704b0df838599f15eb4d6
Jon Sweeney <jonsweeney26@gmail.com>
2021-11-26 16:03:27 -0800

    Initial Commit

---
+++ AbusiveWeather/AbusiveWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  AbusiveWeatherApp.swift
+//  AbusiveWeather
+//
+//  Created by Jonathan Sweeney on 11/26/21.
+//
+
+import SwiftUI
+
+@main
+struct AbusiveWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
